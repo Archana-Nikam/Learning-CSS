0ef0401929afd97001eb9904733c4f8b9744b765
Archana-Nikam <archananikam827@gmail.com>
2024-03-12 23:11:17 +0530

    learning data types in javascript

---
+++ first.js
@@ -0,0 +1,10 @@
+// learning data types in javascript
+// 1)This data type is used to represent a sequence of characters
+
+var str = "Akash Sharma"; //using double quotes
+var str2 = 'David Matt'; //using single quotes
+
+// 2)This datatype is used to represent numerical values.
+
+var x = 25; //without decimal
+var y = 10.6; //with decimal
